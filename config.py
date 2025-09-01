b0324f750e00dda6333b172fcea8e99d57f59400
Manhcuongdzcuti <caothithuhien123mx@gmail.com>
2025-09-01 09:58:12 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# ON = tool hoạt động, OFF = tool bảo trì
+TOOL_STATUS = "on"   # hoặc đổi thành "off" khi bảo trì
+
+# Danh sách thông báo từ admin (in ra trong tool)
+UPDATE_MESSAGES = [
+    "Chào mừng bạn đến với Tool Golike!",
+    "Nếu gặp lỗi hãy liên hệ Admin để được hỗ trợ.",
+    "Phiên bản 3.2.5 đã cập nhật thêm LinkedIn Auto Job."
+]
